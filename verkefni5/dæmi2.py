821bf78684d26864f83b6629c5731c2212f7cbe9
sigurdurBl <sigurdureg210@nemi.tskoli.is>
2019-03-12 12:21:25 +0000

    verk2

---
+++ verkefni5/dæmi2.py
@@ -0,0 +1,58 @@
+from math import *
+class Vigur:
+
+    # Smiður frumstillir x og y hnit vigurs eftir parametrum
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+
+    # Fall sem skrifar hnit vigurs á skjá
+
+    def prenta(self):
+        print("< %s, %s >" %(self.x,self.y))
+
+
+    # Fall sem skilar lengd
+    def lengd(self):
+        return sqrt(self.x**2+self.y**2)
+
+    # Fall sem skilar hallatölu
+    def halli(self):
+        return (self.y/self.x)
+
+
+    # Fall sem skilar þvervigri
+    def þvervigur(self):
+        return Vigur(-self.y,self.x)
+
+    # Fall sem skilar stefnuhorni
+    def stefnuhorn(self):
+        return
+
+
+    # Fall sem tekur vigur sem parameter og skilar horni milli vigra
+    def horn(self,v):
+        return
+
+
+    # Fall sem tekur vigur sem parameter og skilar summu vigri
+    def summa(self,v):
+        return
+
+# Keyrsluforrit
+v1 = Vigur(3,4)
+v1.prenta()
+print("Lengd: ", v1.lengd())
+print("Halli: ", v1.halli())
+
+vþ = v1.þvervigur()
+print("tvervigur: "),
+vþ.prenta()
+print("Stefnuhorn: ", v1.stefnuhorn())
+v2 = Vigur(-3,1)
+print("Horn milli vigra: " , v2.horn(v1))
+v3 = v1.summa(v2)
+print("Summa: "),
+v3.prenta()
+
